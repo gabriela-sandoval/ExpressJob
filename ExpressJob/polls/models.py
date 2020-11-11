d29e73c1e2e9937fa28aab3167cf7334c87bdfff
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2020-11-11 14:14:38 -0600

    Configuración de base de datos

--- ExpressJob/polls/models.py
+++ ExpressJob/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido_paterno = models.CharField(max_length=100)
+    apellido_materno = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10)
+    direccion = models.CharField(max_length=100)
+    def __str__(self):
+        return self.persona_text
+
+class Usuario(models.Model):
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    nombre_usuario = models.CharField(max_length=30)
+    contrasenia = models.CharField(max_length=20)
+    def __str__(self):
+        return self.usuario_text
