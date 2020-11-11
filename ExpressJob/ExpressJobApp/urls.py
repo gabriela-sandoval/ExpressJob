f0dee765ebdfaff7afeb5440fc5b5bf629ead636
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2020-11-10 21:50:08 -0600

    Eliminación de todos los archivos generados

--- ExpressJob/ExpressJobApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from ExpressJobApp import views
-
-urlpatterns = [
-    path('', views.inicio, name="Inicio"),
-    path('login', views.login, name="Login"),
-    path('registro', views.registro, name="Registro"),
-    path('principal', views.principal, name="Principal"),
-]
