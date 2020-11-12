ba27c59d5b5d30700612dea79dfd49c6fe2443bb
Tapia <zs17012957@estudiantes.uv.mx>
2020-11-11 21:42:57 -0600

    Se crea carpeta para generar la ubicación de archivos staticos

--- ExpressJob/polls/views.py
+++ ExpressJob/polls/views.py
@@ -5,5 +5,8 @@ from .models import Persona
 
 def index(request):
     latest_persona_list = Persona.objects.order_by('nombre')
-    output = ', '.join([p.nombre for p in latest_persona_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_persona_list': latest_persona_list,
+    }
+    return HttpResponse(template.render(context, request))
