59a8d337563541a6914d97bdffb567b08cf43d7f
Tapia <zs17012957@estudiantes.uv.mx>
2020-11-13 13:56:17 -0600

    Se agrega vista para template login

--- ExpressJob/polls/views.py
+++ ExpressJob/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Persona
+from django.shortcuts import render, redirect
 
 
 def index(request):
@@ -10,3 +11,5 @@ def index(request):
         'latest_persona_list': latest_persona_list,
     }
     return HttpResponse(template.render(context, request))
+def login(request)
+    return render(request, "/login.html")
